17b009699f3005f35b0b2bd7f81d17c3a5502f7c
Hong Ngoc <ngocnguyen71e11@gmail.com>
2022-04-14 16:12:39 +0700

    commit report

---
+++ BTLLTHSK_FINAL/Report.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CrystalDecisions.CrystalReports.Engine;
+namespace BTLLTHSK_FINAL
+{
+    public partial class Report : Form
+    {
+        public Report()
+        {
+            InitializeComponent();
+        }
+
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+            ReportDocument reportDocument = new ReportDocument();
+            reportDocument.Load(@"C:\Users\Windows 10\Source\Repos\Laptrinhsukien1\BTLLTHSK_FINAL\rpHoaDon.rpt");
+            crystalReportViewer1.ReportSource = reportDocument;
+            crystalReportViewer1.Refresh();
+        }
+
+        private void Report_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
